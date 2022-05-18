a0f5d7d495055a22904ef4472a53de3f7462baf0
Ying-Jung-Chien <64266416+Ying-Jung-Chien@users.noreply.github.com>
2022-05-19 00:39:18 +0800

    database test

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { View, Text } from "react-native";
+import { getDatabase, ref, onValue } from 'firebase/database';
+import '../../firebase'
 
 export default function HomeScreen() {
-   return (
+  const db = getDatabase();
+  const reference = ref(db, 'account/' + "108010013");
+  var nickname;
+  onValue(reference, (snapshot) => {
+    nickname = snapshot.val().nickname;
+  });
+  return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{fontSize:16,fontWeight:'700'}}>Home Screen</Text>
+        <Text style={{fontSize:16,fontWeight:'100'}}>Home Screen</Text>
+        <Text style={{fontSize:16,fontWeight:'100'}}>{nickname}</Text>
     </View>
-   );
+  );
  }
