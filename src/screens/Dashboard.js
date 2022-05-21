2a1c1ecf29b1a8f37f91a52350e3e1d176940235
Ying-Jung-Chien <64266416+Ying-Jung-Chien@users.noreply.github.com>
2022-05-21 23:45:42 +0800

    pass studentID

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -14,20 +14,22 @@ import ApplicationScreen from './Application'
 
 const Drawer = createDrawerNavigator();
 
-export default function Dashboard({ navigation }) {
+export default function Dashboard({ navigation, route }) {
+  const {studentID} = route.params;
+  console.log("Dashboard : ", studentID);
   return (
     <Drawer.Navigator useLegacyImplementation initialRouteName="Home" drawerContent={props => {
       return (
         <DrawerContentScrollView {...props}>
           <DrawerItemList {...props} />
-          <DrawerItem label="Logout" onPress={() => props.navigation.reset({index: 0, routes: [{ name: 'StartScreen' }],})} />
+          <DrawerItem label="Logout" onPress={() => props.navigation.reset({index: 0, routes: [{ name: 'LoginScreen' }],})} />
         </DrawerContentScrollView>
       )
     }}>
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Log" component={LogScreen} />
-      <Drawer.Screen name="Chatroom" component={ChatroomScreen} />
-      <Drawer.Screen name="Application" component={ApplicationScreen} />
+      <Drawer.Screen name="Home" component={HomeScreen} initialParams={{ studentID: studentID }}/>
+      <Drawer.Screen name="Log" component={LogScreen} initialParams={{ studentID: studentID }}/>
+      <Drawer.Screen name="Chatroom" component={ChatroomScreen} initialParams={{ studentID: studentID }}/>
+      <Drawer.Screen name="Application" component={ApplicationScreen} initialParams={{ studentID: studentID }}/>
     </Drawer.Navigator>
   )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,23 @@
 import * as React from 'react';
+import { useState, createContext, useContext } from 'react'
 import { View, Text } from "react-native";
 import { getDatabase, ref, onValue } from 'firebase/database';
 import '../../firebase'
 
-export default function HomeScreen() {
+
+export default function HomeScreen({route}) {
+  const {studentID} = route.params;
+  console.log("studentID : ", studentID);
   const db = getDatabase();
   const reference = ref(db, 'account/' + "108010013");
   var nickname;
-  onValue(reference, (snapshot) => {
-    nickname = snapshot.val().nickname;
+  onValue(reference, async (snapshot) => {
+    nickname = await snapshot.val().nickname;
+    console.log("succesful");
   });
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{fontSize:16,fontWeight:'100'}}>Home Screen</Text>
-        <Text style={{fontSize:16,fontWeight:'100'}}>{nickname}</Text>
+        <Text style={{fontSize:16,fontWeight:'700'}}>Home Screen</Text>
     </View>
   );
  }
